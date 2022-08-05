84526a60feaf9b4df31b87c59e27d41357969b07
Jeff <tibame201020@gmail.com>
2022-08-05 15:45:29 +0800

    fix sh

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8-jdk-alpine
 COPY ./target/dockerTest.jar /app/dockerTest.jar
 WORKDIR /app
-CMD ["java", "-jar", "dockerTest.jar"]
+ENTRYPOINT ["java", "-jar", "dockerTest.jar"]
--- baseDockerCmd.sh
+++ baseDockerCmd.sh
@@ -1,12 +1,4 @@
-@SET /P dockerCmd=docker cmd :
-@echo cmd:%dockerCmd%
 
-@SET cmd=""
-if "%dockerCmd%"=="build" (
-	@SET cmd="docker build -t imageName ."
-)
-if "%dockerCmd%"=="run" (
-	@SET cmd="docker run -d --rm --name Name -p outerport:innerport image"
-)
-@echo %cmd%
+echo "docker build -t imageName ."
+echo "docker run -d --rm --name Name -p outerport:innerport image"
 
